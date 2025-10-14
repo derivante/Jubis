619b9f3dbd707c5fa68e7b3b56689d11fc8834a9
Sumo <117795249+derivante@users.noreply.github.com>
2025-10-14 18:09:41 -0300

    Add files via upload

---
+++ src/js/config/letters.js
@@ -0,0 +1,281 @@
+export const so_pra_eu_lembrar_o_tamanho_maximo_das_msg = `
+    ...................................................................................................................................
+
+    ██████╗ ██╗   ██╗████████╗███████╗████████╗██╗██╗     ██╗         ██╗   ██╗  ██████╗  ██████╗ ██████╗ ███████╗
+    ██╔══██╗██║   ██║╚══██╔══╝██╔════╝╚══██╔══╝██║██║     ██║         ██║   ██║  ╚════██╗██╔═████╗╚════██╗██╔════╝
+    ██████╔╝██║   ██║   ██║   ███████╗   ██║   ██║██║     ██║         ██║   ██║   █████╔╝██║██╔██║ █████╔╝███████╗
+    ██╔══██╗██║   ██║   ██║   ╚════██║   ██║   ██║██║     ██║         ╚██╗ ██╔╝  ██╔═══╝ ████╔╝██║██╔═══╝ ╚════██║
+    ██████╔╝╚██████╔╝   ██║   ███████║   ██║   ██║███████╗███████╗     ╚████╔╝██╗███████╗╚██████╔╝███████╗███████║
+    ╚═════╝  ╚═════╝    ╚═╝   ╚══════╝   ╚═╝   ╚═╝╚══════╝╚══════╝      ╚═══╝ ╚═╝╚══════╝ ╚═════╝ ╚══════╝╚══════╝
+                                                                                                                        
+    ...................................................................................................................................
+
+`
+
+export const Julia = `
+    _________________________________________________________________________________________________________________________________
+    
+    Julia <Julia@fireheart.com>                                                              12 de out. de 2025, 01:21 
+
+    Oi amiga, feliz seu dia! 
+
+   Sei que provavelmente já mandei um texto carinhoso de felicitações mas, mais uma vez venho aqui pra lembrar que eu te amo muito 
+   e te admiro, por quem você é. 
+   você foi a amizade mais improvável da minha vida, e a que mais deu certo. obrigada por tudo e por sempre estar aqui. 
+
+   Que venham mais 50 anos para comemorarmos juntas. 
+
+   eu subiria sob montanha e ficaria trancada em um caixão de ferro por você. 
+
+   𝗌𝗎𝖺 𝖼𝗈𝗋𝖺çã𝗈 𝖽𝖾 𝖿𝗈𝗀𝗈.
+
+     Assinado: <3                                                                                                                                                                  
+    
+    ________________________________________________________________________________________________________________________________
+
+    [ ↰ Responder ]  |  [ Encaminhar ↱ ]    
+`;
+
+export const Victoria = `
+    _________________________________________________________________________________________________________________________________
+
+    victoria <v.santosz_@la.com>                                                                              10 de out. de 2025, 16:49 
+
+    Feliz niver jujubis!!! Que nessa fase nova da sua vida você alcance todos os seus objetivos, 
+    e que esse ano novo que começa na sua vida seja cheio de grandes realizações,você ilumina os dias de muitas pessoas 
+    e só merece coisas boas!! 
+    Amo você e a nossa amizade, você tem um pedaço do meu coração pra sempre. 
+    Feliz vida, que venham mais muitos anos de comemorações e sempre juntas!! 
+    
+    Assinado: Victoria
+    _________________________________________________________________________________________________________________________________
+
+    [ ↰ Responder ]  |  [ Encaminhar ↱ ]    
+ `;
+
+export const Gui = `
+    _________________________________________________________________________________________________________________________________
+    
+    Gui <Gui@cruz.com>                                                                    24 de jan. de 2004, 04:13 
+
+    Juuuu, feliz aniversário meu bem! Que você tenha um ano próspero e cheio de realizações. 
+    Beijo grande!
+
+    Assinado: Gui                                                             
+    ________________________________________________________________________________________________________________________________
+
+    [ ↰ Responder ]  |  [ Encaminhar ↱ ] 
+
+`;
+
+export const Manu = `   
+    _________________________________________________________________________________________________________________________________
+    
+    Manu <Manu@De.com>                                                                   10 de out. de 2025, 16:49 
+
+    Feliz aniversário jujubis, que Deus abençoe grandemente sua vida e que você possa realizar todos os seus sonhos!! 
+    Aproveite muito seu dia
+
+    Assinado: Manu
+    
+    ________________________________________________________________________________________________________________________________
+
+    [ ↰ Responder ]  |  [ Encaminhar ↱ ] 
+`;
+
+export const Brenda = `
+    _________________________________________________________________________________________________________________________________
+    
+    Breanda <Bre@nda.com>                                                                                09 de out. de 2025, 23:41 
+
+    Parabéns, juuu!!! Que esse novo ano seja incrível e que você realize todos os seus objetivos!! 
+    Muita saúde, paz e muitos anos de vida!! S2 S2
+
+    Assinado: Brenda
+  
+  ___________________________________________________________________________________________________________
+
+    [ ↰ Responder ]  |  [ Encaminhar ↱ ] 
+`;
+
+
+export const Yas = `
+    _________________________________________________________________________________________________________________________________
+    
+    Yas <Y@smim.com>                                                               2 de out. de 2025, 23:41 
+    
+     "seja leal a você"
+    
+    Assinado: Yas
+    ________________________________________________________________________________________________________________________________
+
+    [ ↰ Responder ]  |  [ Encaminhar ↱ ]     
+`;
+
+
+export const Gustavo = `
+    _________________________________________________________________________________________________________________________________
+    
+    Gustavo <Gus@gue.IceCream>                                                                      6 de out. de 2025, 04:20 
+
+    PARABÉNS BOBONA!!!
+
+    Oie, primeiramente queria te desejar um feliz aniversário, te lembrar que você é extremamente especial pra mim, 
+e que eu fico muito feliz de ter você do meu lado, que você seja muito feliz, se adpte bem ao novo emprego, 
+e que comece a comer direito pelo amor de deus!! 
+É seu dia (e dos professores também, mas a gente releva), então aproveita, te amo bobona.
+    
+
+    Assinado: Gustavo
+    ________________________________________________________________________________________________________________________________
+
+    [ ↰ Responder ]  |  [ Encaminhar ↱ ] 
+
+`;
+
+export const Luna = `    
+    _________________________________________________________________________________________________________________________________
+    
+    Luna <moc.Luna@distance.com>                                                                   04 de out. de 2025, 21:08
+
+    Oi minha vida!! não tem palavras que possam algum dia descrever o quanto você é especial, te conhecer e te amar faz de mim a pessoa mais grata e feliz desse mundo, 
+    espero que o meu amor consiga sempre ultrapassar esses 3.058,00 km de distância e te alcançar 
+    todos os dias como você me alcança, acredito fielmente que ninguém nesse planeta me entenda como você, 
+    saiba que não importa o que aconteça você vai ter pra sempre uma amiga em mim, 
+    sinto sua falta sempre e não vejo a hora de poder te abraçar, desejo mais do que tudo que todo sucesso te persiga e te ataque, 
+    você merece tudo! te amo muito esposa ♥️✨🎉 #Dijuh4ever 
+
+    Assinado: Luna
+                                        
+    ________________________________________________________________________________________________________________________________
+
+    [ ↰ Responder ]  |  [ Encaminhar ↱ ] 
+`;
+
+export const Murilo = `
+    _________________________________________________________________________________________________________________________________
+    
+    Murilo <mur@lo.com>                                                                      04 de out. de 2025, 21:10 
+
+    Chero,
+
+    Muito obrigado por ser essa companhia maravilhosa que você tem sido nesses últimos 9 anos. 
+   Apesar da distância, saiba que você pode contar comigo pra absolutamente qualquer coisa e que você tem um lugar 
+   muito bem guardado no meu coração. 
+   Feliz aniversário! Amo você
+    
+   Assinado: Murilo
+                                                   
+                                                   
+   
+    ________________________________________________________________________________________________________________________________
+
+    [ ↰ Responder ]  |  [ Encaminhar ↱ ]     
+`;
+
+export const Felipe = `
+    _________________________________________________________________________________________________________________________________
+    
+    Felipe <Fe@li.pe.com>                                                                                13 de out. de 2025, 16:33 
+
+   feliz aniversário!
+   que o seu dia seja cheio de coisas boas, risadas sinceras e gente que te quer bem.
+   que venham muitos outros anos cheios de momentos incríveis
+
+
+    Assinado: Felipe
+
+    ________________________________________________________________________________________________________________________________
+
+    [ ↰ Responder ]  |  [ Encaminhar ↱ ]     
+
+`;
+
+export const Giovana = `
+    _________________________________________________________________________________________________________________________________
+    
+    Gi <Gio@Vanna.com>                                                                             03 de out. de 2025, 02:30 
+
+       Ju, tu é uma das pessoas mais incríveis que já conheci. É inspiradora, uma amiga excepcional, uma pessoa que leva luz por onde passa, 
+    e alguém que tá sempre pronta a oferecer o melhor que tem. Sua inteligência faz todos ao seu redor te admirarem, 
+    e a forma que você sempre consegue mudar um ambiente completamente para o melhor, é o que faz as pessoas quererem 
+    estar o tempo todo próximos de ti. 
+    Um textinho ainda é muito pouco para conseguir compactar todas as qualidades que você tem 
+    (além das que você adiciona na vida das outras pessoas),
+    e tá, beleza, você também é muito bonita, mas se fosse possível 
+    transformar sua beleza externa e interna em um .zip do drive, nem o plano pago do 
+    Google tankaria o peso do tamanho do arquivo.
+
+    Bjo, te amo muito, e saiba que você é uma das melhores pessoas na minha vida! sz
+
+    Assinado: Giovanna
+                                                                                                                 
+    ________________________________________________________________________________________________________________________________
+
+    [ ↰ Responder ]  |  [ Encaminhar ↱ ] 
+
+`;
+
+export const Sumo = `
+    _________________________________________________________________________________________________________________________________
+    
+    Sumo <sumo>                                                                                             15 de out. de 2025, 03:29 
+
+   ok, essa é a verdadeira mesmo.
+
+   então, vamo lá.
+eu sei que nem sempre tenho sido a melhor pessoa contigo muitas vezes por causa de ciúmes. De verdade, eu não queria ser assim. 
+não é algo que eu faça por querer, só... às vezes me magoa, e eu acabo reagindo mal.
+
+fazer isso aqui foi um desafio pessoal. não pela parte de montar o site (isso até foi tranquilo o problema foi upar kkkkk)
+mas porque mexe comigo, sabe? é meio complicado de explicar, mas acho que tu entende.
+
+de qualquer forma, eu te amo muito.
+às vezes eu só não sei lidar direito com a situação atual, então acabo ficando estranho do nada mas nunca é por mal.
+
+penso muito no que seria o ideal pra nós, e também no que tu pensa de mim.
+odeio ser o tipo de pessoa que fica insegura com tudo, mas, pra mim, é difícil mesmo.
+
+já falei várias vezes o quanto foi incrível aquele tempo nosso o quanto me diverti contigo, o quanto foi especial.
+não sei bem o que quero concluir com isso… só que sinto falta.
+
+mas tá tudo bem. as coisas vão melhorar em algum momento, eu sei.
+chega de falar do que eu sinto
+eu te amo, e pronto.
+
+espero que tenha gostado da surpresa, e que aproveite muito o teu dia.
+eu sempre vou estar aqui, se tu quiser.
+
+te amo, meu anjo.
+
+
+    Assinado: Sumo
+                                                                                                                                                                       
+    ________________________________________________________________________________________________________________________________
+
+    [ ↰ Responder ]  |  [ Encaminhar ↱ ]     
+`;
+
+export const Arthur = `
+    _________________________________________________________________________________________________________________________________
+    
+    Arthur <sumo@escr.eve>                                                                       15 de out. de 2025, 00:00 
+
+    Oii Ju,
+    teu dia, hein? S2
+
+    Queria poder registrar ele de um jeito especial esse ano, então fui atrás de todo mundo que consegui reunir.
+    No começo achei que tinha tido uma ideia super original chamando a Luna e o Murilo também mas aí você me contou que já tinham feito 
+algo parecido antes, então agora tem mais uma lembrança pra somar ai no estoque 
+Não sei se tenho muito o que falar, você só foi a melhor pessoa que pude ter do meu lado nesse ultimos mês, foi bem divertido,
+só quero que tu aproveite o teu dia hoje, e que a tal depressão de aniversário não te alcance dessa vez.
+
+    É isso.
+    Te amo muito.
+
+    Assinado: Arthur
+    ________________________________________________________________________________________________________________________________
+
+    [ ↰ Responder ]  |  [ Encaminhar ↱ ]     
+`;
+
---
+++ src/js/terminal/terminal.js
@@ -0,0 +1,144 @@
+import {banner, menuletters, n_banner, comandos, respond, foward} from "../config/content.js";
+import {Arthur, Julia, Victoria, Gui, Manu, Brenda, Yas, Giovana, Gustavo, Luna, Murilo, Felipe, Sumo} from "../config/letters.js";
+import { scrollToBottom } from '../handlers/utils.js';
+
+
+export async function butstill() {
+	const terminalOutput = document.getElementById("terminal-output");
+	const welcomeMessage2 = n_banner;
+	const newOutputLine = document.createElement("div");
+	terminalOutput.appendChild(newOutputLine);
+	await animateText(newOutputLine, welcomeMessage2);
+	scrollToBottom();
+}
+
+export async function loading() {
+	const terminalOutput = document.getElementById("terminal-output");
+	const welcomeMessage1 = banner;
+	const newOutputLine = document.createElement("div");
+	terminalOutput.appendChild(newOutputLine);
+	await animateText(newOutputLine, welcomeMessage1);
+	scrollToBottom();
+
+	setTimeout(() => {
+		document.getElementById("terminal-output").innerHTML = "";
+	}, 2000)
+
+	setTimeout(() => {
+		butstill();
+	}, 2000)
+}
+
+export function processCommand(inputText) {
+	
+	const terminalInput = document.getElementById("terminal-input");
+	const userCommand = terminalInput.textContent;
+	let response = '';
+
+	switch (inputText.toLowerCase()) {
+
+		case "clear":	
+			document.getElementById("terminal-output").innerHTML = n_banner;
+			return "";
+			// Aqui podemos manipular o que o juh vai escrever e ter acesso
+		case "menu":
+			document.getElementById("terminal-output").innerHTML = "";
+			return userCommand + "\n" + menuletters ;
+
+	  	case "comandos":
+			return userCommand + "\n" + comandos;
+
+		case "mensagens":
+			return userCommand + "\n" + menuletters;
+
+		case "responder":
+			window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', "_blank");
+			return userCommand + "\n" + respond;
+
+		case "encaminhar":
+			window.open('https://www.youtube.com/watch?v=NNv2RHR62Rs', "_blank");
+			return userCommand + "\n" + foward;
+
+		case "julia":
+			return userCommand + "\n" + Julia;
+
+		case "victoria":
+			return userCommand + "\n" + Victoria;
+		
+		case "gui":
+			return userCommand + "\n" + Gui;
+		
+		case "manu":
+			return userCommand + "\n" + Manu;
+
+		case "sumo":
+			return userCommand + "\n" + Sumo;
+
+		case "brenda":
+			return userCommand + "\n" + Brenda;
+
+		case "yas":
+			return userCommand + "\n" + Yas;
+
+		case "gustavo":
+			return userCommand + "\n" + Gustavo;
+
+		case "luna":
+			return userCommand + "\n" + daniel;
+		
+		case "giovanna":
+			return userCommand + "\n" + Giovana;
+		
+		case "luna":
+			return userCommand + "\n" + Luna;
+
+		case "murilo":
+			return userCommand + "\n" + Murilo;
+
+		case "felipe":
+			return userCommand + "\n" + Felipe;
+
+		case "arthur":
+			return userCommand + "\n" + Arthur;
+	
+		default:
+			return userCommand + "\n" + `Unknown command: ${inputText}`;
+	}
+	return response;
+}
+
+let userInteracted = false;
+document.addEventListener("click", () => {
+	userInteracted = true;
+});
+  
+export async function animateText(element, text, delay = 10, terminalInput, inputPrefix) {
+	if (terminalInput) {
+		terminalInput.contentEditable = "false";
+		if (inputPrefix) inputPrefix.style.display = "none";
+	}
+	
+	const speedFactor = text.length <= 50 ? 1 : text.length <= 100 ? 10 : 20;
+	const adjustedDelay = delay / speedFactor;
+
+	for (const char of text) {
+		element.textContent += char;
+		scrollToBottom();
+
+		if (userInteracted) {
+
+		typingSound.currentTime = 0;
+		typingSound.play().catch((error) => {
+		   console.error("Error playing typing sound:", error);
+		});
+
+		}
+
+		await new Promise((resolve) => setTimeout(resolve, adjustedDelay));
+	}
+
+	if (terminalInput) {
+		terminalInput.contentEditable = "true";
+		if (inputPrefix) inputPrefix.style.display = "inline";
+	}
+}
