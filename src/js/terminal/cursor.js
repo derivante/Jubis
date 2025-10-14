e993910210bee5b4ed1dfb21edf785724f81868e
Sumo <117795249+derivante@users.noreply.github.com>
2025-10-14 18:23:18 -0300

    Delete src directory

--- src/js/terminal/cursor.js
+++
@@ -1,9 +0,0 @@
-export function initCursor() {
-  const terminalInput = document.getElementById("terminal-input");
-  const cursor = document.createElement("span");
-  cursor.classList.add("cursor");
-  terminalInput.parentElement.appendChild(cursor);
-
-  terminalInput.addEventListener("focus", () => cursor.classList.add("cursor"));
-  terminalInput.addEventListener("blur", () => cursor.classList.remove("cursor"));
-}
